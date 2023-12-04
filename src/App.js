ea8065431f0e1f91385ccad8b62d355ed0fc2010
rice-warlord <barrettbarrettbarrett@protonmail.com>
2023-12-04 00:20:10 -0500

    finished css and functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { increaseCount, decreaseCount, resetCount } from './redux/action/counterAction';
 
 function App() {
   const count = useSelector(state => state.count);
   const dispatch = useDispatch();
 
+const increment = () => {
+  dispatch(increaseCount())
+}
+
+const decrement = () => {
+  dispatch(decreaseCount())
+}
+
+const reset = () => {
+  dispatch(resetCount())
+}
+
   return (
     <div className="App">
       <h1 className='app-heading'>COUNTER APP</h1>
       <div className='btn-div'>
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
       </div>
       <h1 className='app-count'>{count}</h1>
     </div>
